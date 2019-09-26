4991f247ff155a6e8efb1412763b365b51fb7eb7
CuiHao <203447262@qq.com>
2019-09-26 17:57:27 +0800

    新建项目

--- app/src/main/java/com/example/myapplication/base/BaseActivity.java
+++ app/src/main/java/com/example/myapplication/base/BaseActivity.java
@@ -13,8 +13,7 @@ public abstract  class BaseActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(initLayoutId());
         //保留顶部标题栏距离
-        StatusBarUtil.setRootViewFitsSystemWindows(this, true);
-        //设置状态栏透明
+        StatusBarUtil.setRootViewFitsSystemWindows(this, true);        //设置状态栏透明
         StatusBarUtil.setTranslucentStatus(this);
         if (!StatusBarUtil.setStatusBarDarkTheme(this, true)) {
             //如果不支持设置深色风格 为了兼容总不能让状态栏白白的看不清, 于是设置一个状态栏颜色为半透明,
